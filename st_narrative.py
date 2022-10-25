39e9410f461e837fed57bfef427c4a845009dcb9
yoonsen <yoonsen@gmail.com>
2022-10-25 17:59:37 +0200

    up to date

--- st_narrative.py
+++ st_narrative.py
@@ -25,6 +25,8 @@ st.markdown("""Les mer på [DHLAB-siden](https://nb.no/dh-lab)""")
 
 st.title('Ord i tekst - Narrative buer')
 
+window = st.sidebar.number_input("Størrelse på tekst det telles i (vindu)", min_value = 300, value=500)
+pr = st. sidebar.number_input("Antall steg mellom hvert vindu", min_value = 100, value=100)
 
 stikkord = st.text_input('Angi noen stikkord for å forme et utvalg tekster, og velg fra listen under')
 period = st.slider('Begrens listen til år', 1800, 2022, (1980, 2020))
@@ -45,7 +47,7 @@ words = st.text_input('Angi ord som skal telles', '. ,')
 words = words.split()
 
 try:
-    dispersion = get_dispersion(urn=urn, wordbag=words, window=1000, pr=100)
+    dispersion = get_dispersion(urn=urn, wordbag=words, window=window, pr=pr)
     st.line_chart(dispersion.dispersion)
 except:
     st.write(f"Noe gikk galt med {' '.join(valg.split()[:-1])}, prøve et annet dokument")
